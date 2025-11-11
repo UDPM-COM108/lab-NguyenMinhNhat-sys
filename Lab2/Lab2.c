079935111e2d16489288099db93ea6cf6973de44
Nguyen MInh Nhat <Nhatcoilk147@gmail.com>
2025-11-11 21:57:07 +0700

    Nop Lab 2

---
+++ Lab2/Lab2.c
@@ -0,0 +1,32 @@
+
+/*#include <stdio.h>
+    int main () {
+    int a,b;
+    printf ("nhap vao 2 so nguyen de tinh tong va hieu:  ");
+    scanf("%d %d", &a,&b);
+
+    printf("hieu = %d\n", a - b);
+    printf("tong = %d", a + b);
+    return 0;
+}*/
+/*#include <stdio.h>
+    int main () {
+    float weight;
+    float height;
+    printf("nhap chieu rong va chieu dai");
+    scanf("%f %f", &weight, &height);
+
+    printf("dien tich HCN = %2.f", height * weight);
+    printf("Chu vi HCN = %2.f", (height + weight)*2);
+    return 0;
+    }*/
+/*#include <stdio.h>
+#define PI 3.14 
+int main() {
+    float banKinh;
+    printf("nhap vao ban kinh hinh tron");
+    scanf("%f", &banKinh);
+
+    printf("dien tich = %.2f\n", (banKinh * banKinh) * PI);
+    printf("chu vi = %.2f", PI * 2* banKinh);
+}*/
